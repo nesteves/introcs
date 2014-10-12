3e62b4d28ccbec5231b2dad5bcc9f78892f809e8
Nuno Esteves <nesteves@users.noreply.github.com>
2014-10-13 00:28:50 +0100

    Add a prime number generator method

--- scripts/introcs/generators.py
+++ scripts/introcs/generators.py
@@ -14,20 +14,45 @@ def gen_fibonacci():
         fib1 = fib2
         fib2 = next
 
+
+def genPrimes():
+    yield 2
+    yield 3
+    primes = [2, 3]
+    n = 3
+    while True:
+        n += 1
+        is_prime = True
+        for i in primes:
+            if n % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(n)
+            yield n
+
+
 def gen_listitems(li):
     for item in li:
         yield item
 
 
 if __name__ == '__main__':
+
+    # Fibonacci generator
     fib = gen_fibonacci()
     print [fib.next() for i in range(20)]
 
-    a = [i for i in range(20)]
+    # Prime number generator
+    pri = gen_primes()
+    print [pri.next() for i in range(20)]
 
+    a = [i for i in range(20)]
+    b = []
     for i in gen_listitems(a):
-        b = i
+        # Changing i does not effect the original list a
         i = 11
-        print b, i
+        b.append(i)
 
     print a
+    print b
